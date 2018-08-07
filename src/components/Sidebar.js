cd947da06d38a35505b5d40c88b9ae528ff2c6f8
Silvia <stoianova.silvia@yahoo.com>
2018-08-07 23:02:57 +0300

    add: list of titles on the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import landmarks from '../data/landmarks';
 import '../App.css';
 
 
 class Sidebar extends Component{
 
+
+
+
     render(){
         return(
                 <nav id='sidebar'>
+
                     <div className='sidebar-content'>
-                        <div className="search-location-input-wrapper">
+                        <div className="input-wrapper">
                             <input
                                 type='text'
                                 placeholder = 'Filter by landmark'
@@ -18,7 +23,12 @@ class Sidebar extends Component{
                         </div>
                         <div id='locations'>
                             <ol className='location-list'>
-                                <li key='id 'className='location'>list1</li>
+                                {landmarks.map((landmark)=>(
+                                    <li key={landmark.title} className='location'>
+                                        {landmark.title}
+                                    </li>))
+                                }
+                                
                             </ol>
                         </div>
                     </div>
